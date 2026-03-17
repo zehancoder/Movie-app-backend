096f1a73a3be3bead92212ea9d7e3618458d9dc6
zehancoder <zehanjh93@gmail.com>
2026-03-17 17:49:16 +0600

    fix another bug for deployment

--- backend/src/routes/getMe.route.js
+++ backend/src/routes/getMe.route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const userIdentifyMiddleWare = require('../middlewares/userIdentify.middleware');
-const getMeController = require('../controllers/getme.controller');
+const getMeController = require('../controllers/getMe.controller');
 const getMeRouter = express();
 getMeRouter.get('/get-me', userIdentifyMiddleWare, getMeController  );
 module.exports= getMeRouter
